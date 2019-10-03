3c062899907e25866865c94c65e9be282887eea2
arham-lodha <arhamsmita@gmail.com>
2019-10-02 22:08:49 -0500

    Added NodeTester main class

--- src/NodeTester.java
+++ src/NodeTester.java
@@ -1,9 +1,29 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class NodeTester {
 
-    public static void main(String[] args) {
-        Scanner file = new Scanner("actor.dat");
+    public static void main(String[] args) throws Exception {
+
+        Scanner file = new Scanner(new File("actor.dat"));
+        Actor head = new Actor(file.nextLine());
+
+        while (file.hasNext()){
+            Actor newActor = new Actor(file.nextLine());
+            newActor.setNextPtr(head);
+            head = newActor;
+        }
+
+        Actor current = head;
+
+        while (current != null){
+            System.out.println(current.getName());
+            current = current.getNextPtr();
+        }
+
+        System.out.println(head);
+
     }
+
 
 }
